694f6214f9672e1d95001882836e90f76e7f1436
Akeem King <akeemtlking@gmail.com>
2021-08-16 11:55:25 -0400

    Add eslint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import {
   Divider,
   AppBar,
   Toolbar,
-	Button,
+  Button,
   Drawer,
-	TextField,
-	FormControl,
-	FormHelperText,
+  TextField,
+  FormControl,
+  FormHelperText,
   IconButton,
   List,
   ListItem,
   ListItemText,
   ListItemIcon,
-	MenuItem,
-	Select,
-	Table,
+  MenuItem,
+  Select,
+  Table,
   Typography,
-	TableRow,
-	TableCell,
-	TableHead,
-	TableBody,
-	TableContainer
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+  TableContainer,
 } from '@material-ui/core';
 
 import {Sort, Menu, ChevronLeft} from '@material-ui/icons';
@@ -31,105 +31,114 @@ import './App.css';
 
 
 const SortVisualizer = (props) => {
-	const { steps } = props;
-	return (
-		<TableContainer>
-			<Table>
-				<TableHead>
-					<TableRow key='header'>
-						<TableCell colSpan={steps[0].list.length}>Steps</TableCell>
-					</TableRow>
+  const {steps} = props;
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow key='header'>
+            <TableCell colSpan={steps[0].list.length}>Steps</TableCell>
+          </TableRow>
 
-				</TableHead>
-				<TableBody>
-					{steps.map(step => (
-						<TableRow key={step.id}>
-							{step.list.map(value => (
-								<TableCell key={step.id + value}>{value}</TableCell>
-							))}
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	);
+        </TableHead>
+        <TableBody>
+          {steps.map((step) => (
+            <TableRow key={step.id}>
+              {step.list.map((value) => (
+                <TableCell key={step.id + value}>{value}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 
-const requestSort = (inputArray, algorithm) => {
+/**
+ * Sends a POST request to the server to get sorting steps.
+ * @param {string} input  a comma-delimited string containing numbers
+ * @param {string} algorithm  algorithm to use for sorting the numbers
+ * @return {Promise}  the array with the step by step sorting
+ */
+function requestSort(input, algorithm) {
   return new Promise((accept, reject) => {
-		const elements = new Set(inputArray.split(',').filter(element => element !== "" || !isNaN(element)).map(element => parseFloat(element)));
-		const request = new XMLHttpRequest();
-		request.onload = function() {
+    const request = new XMLHttpRequest();
+    request.onload = function() {
       const newSteps = JSON.parse(request.responseText);
-      accept(newSteps)
-		}
-		request.onerror = function(err) {
+      accept(newSteps);
+    };
+    request.onerror = function(err) {
       reject(err);
-		}
-		request.open("POST", "http://127.0.0.1:8080/sort");
-		request.send(JSON.stringify({
-			elements: Array.from(elements),
-			algorithm
-		}));
+    };
+    request.open('POST', 'http://127.0.0.1:8080/sort');
+    const isNotNumber = (element) => element !== '' || !isNaN(element);
+    const elements = input.split(',').filter(isNotNumber);
+    const sortItems = new Set(elements.map((element) => parseFloat(element)));
+    request.send(JSON.stringify({
+      elements: Array.from(sortItems),
+      algorithm,
+    }));
   });
-};
+}
 
 const SortingPlugin = (props) => {
-
-	const [algorithm, setAlgorithm] = useState(props.algorithm);
+  const [algorithm, setAlgorithm] = useState(props.algorithm);
   const [steps, setSteps] = useState(props.steps || []);
 
   // ask server for supported algorithms
-	const supported = ['bubble', 'insertion', 'merge'];
-	if (!supported.includes(algorithm)) throw new Error('Unsupported algorithm passed.');
+  const supported = ['bubble', 'insertion', 'merge'];
+  if (!supported.includes(algorithm)) {
+    throw new Error('Unsupported algorithm passed.');
+  }
 
-	const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
 
-	const handleClick = () => {
+  const handleClick = () => {
     // request sorting operation from server
     requestSort(input, algorithm)
-      .then(newSteps => setSteps(newSteps))
-      .catch(err => console.error(err));
-	};
+        .then((newSteps) => setSteps(newSteps))
+        .catch((err) => console.error(err));
+  };
 
-	return (
-		<div className='center-screen'>
-			<TextField
-				value={input}
-				onChange={event => setInput(event.target.value)}
-				multiline
-				minRows={4}
-				label='Input comma separated integers'
-				id='input-elements'
-			/>
-			<br/>
-			<FormControl>
-				<Select
-					labelId='algorithm-select-label'
-					id="algorithm-select"
-					value={algorithm}
-					onChange={event => setAlgorithm(event.target.value)}
-				>
-					{supported.map(algo => <MenuItem key={algo} value={algo}>{algo}</MenuItem>)}
-				</Select>
-				<FormHelperText>Sorting Algorithm</FormHelperText>
-			</FormControl>
-			<br/>
-			<Button onClick={handleClick}>Submit</Button>
-			{steps.length ? <SortVisualizer steps={steps}/> : null}
-		</div>
-	);
-
+  return (
+    <div className='center-screen'>
+      <TextField
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        multiline
+        minRows={4}
+        label='Input comma separated integers'
+        id='input-elements'
+      />
+      <br/>
+      <FormControl>
+        <Select
+          labelId='algorithm-select-label'
+          id="algorithm-select"
+          value={algorithm}
+          onChange={(event) => setAlgorithm(event.target.value)}
+        >
+          {supported.map((algo) => {
+            return <MenuItem key={algo} value={algo}>{algo}</MenuItem>;
+          })}
+        </Select>
+        <FormHelperText>Sorting Algorithm</FormHelperText>
+      </FormControl>
+      <br/>
+      <Button onClick={handleClick}>Submit</Button>
+      {steps.length ? <SortVisualizer steps={steps}/> : null}
+    </div>
+  );
 };
 
 const App = () => {
-
   // ask server for starting algorithm
   const container = window.document.body || undefined;
 
   const [openDrawer, setDrawerOpen] = useState(false);
-	return (
+  return (
     <div>
       <AppBar
         position="fixed"
@@ -149,32 +158,32 @@ const App = () => {
         </Toolbar>
       </AppBar>
       <nav>
-      <Drawer container={container} variant='temporary' open={openDrawer}>    
-        <div
-        role="presentation"
-        onClick={() => setDrawerOpen(!openDrawer)}
-        onKeyDown={() => setDrawerOpen(!openDrawer)}
-        >
-          <div>
-            <IconButton onClick={() => setDrawerOpen(!openDrawer)}>
-              {<ChevronLeft />}
-            </IconButton>
+        <Drawer container={container} variant='temporary' open={openDrawer}>
+          <div
+            role="presentation"
+            onClick={() => setDrawerOpen(!openDrawer)}
+            onKeyDown={() => setDrawerOpen(!openDrawer)}
+          >
+            <div>
+              <IconButton onClick={() => setDrawerOpen(!openDrawer)}>
+                {<ChevronLeft />}
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              <ListItem button key={'Sort'}>
+                <ListItemIcon>{<Sort/>}</ListItemIcon>
+                <ListItemText primary={'Sort'} />
+              </ListItem>
+            </List>
           </div>
-          <Divider />
-          <List>
-            <ListItem button key={'Sort'}>
-              <ListItemIcon>{<Sort/>}</ListItemIcon>
-              <ListItemText primary={'Sort'} />
-            </ListItem>
-          </List>
-        </div>
-      </Drawer>
+        </Drawer>
       </nav>
       <main style={{flexGrow: 1}}>
         <SortingPlugin algorithm={'bubble'}/>
       </main>
     </div>
-	);
+  );
 };
 
 export default App;
